c7e02d0fad6727efb17f8d163fc7db565eba1d43
danielcabiscol <danielcabiscol@gmail.com>
2020-04-10 09:32:58 +0200

    feat: added function too display on console sizings

---
+++ src/display-layout-sizing.ts
@@ -0,0 +1,19 @@
+export const displayLayoutSizing = (withLog: boolean) => {
+  const doc = document.getElementsByTagName('body')[0];
+  const out = window.outerHeight;
+  const inn = window.innerHeight; //
+  if (doc) {
+    doc.style.setProperty('--window-outer-height', `${out}px`);
+    doc.style.setProperty('--window-inner-height', `${inn}px`);
+    doc.style.setProperty('--body-height', `${doc.clientHeight}px`);
+    //console.log(doc.clientHeight);
+  }
+  if (withLog) {
+    console.log('|---------------------------------------|');
+    console.log('|----------- LAYOUT SIZING -------------|');
+    console.log(`|------ Window outer height: ${out}px -----|`);
+    console.log(`|------ Window inner height: ${inn}px -----|`);
+    console.log(`|--------- Body height: ${doc.clientHeight}px ----------|`);
+    console.log(`|---------------------------------------|`);
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from './display-layout-sizing';
